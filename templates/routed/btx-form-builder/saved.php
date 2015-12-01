2d6805b5c284aaa232e10ea4d7ce37e5c8469eab
Randy Hook <randallhook@gmail.com>
2015-12-01 12:16:50 -0500

    added alternative layout to thanks and saved pages

--- templates/routed/btx-form-builder/saved.php
+++ templates/routed/btx-form-builder/saved.php
@@ -1,1 +1,9 @@
+<?
+if (!empty($form["layout"])) {
+    if (file_exists(SERVER_ROOT . "templates/layouts/" . $form["layout"] . ".php")) {
+        $bigtree["layout"] = $form["layout"];
+    }
+}
+?>
+
 <h1>Your Progress Has Been Saved</h1>
--- templates/routed/btx-form-builder/thanks.php
+++ templates/routed/btx-form-builder/thanks.php
@@ -1,2 +1,10 @@
+<?
+if (!empty($form["layout"])) {
+    if (file_exists(SERVER_ROOT . "templates/layouts/" . $form["layout"] . ".php")) {
+        $bigtree["layout"] = $form["layout"];
+    }
+}
+?>
+
 <h1><?=$thank_you_page_header?></h1>
 <?=$thank_you_page_content?>
